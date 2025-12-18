b25ca1530e90b836af3f9dfe2b9a4ebd87c1145c
Muhammad Hammad Arshad <ihammadarshad@gmail.com>
2025-12-18 23:40:10 +0500

    New Update - 18-12-2025 - 0.4.19

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "XEX-XEX-Game-0.4.17";
+const cacheName = "XEX-XEX-Game-0.4.19";
 const contentToCache = [
-    "Build/Xex Crypto Full Version WebGL Build-V(0.4.17) 28-11-2025.loader.js",
-    "Build/Xex Crypto Full Version WebGL Build-V(0.4.17) 28-11-2025.framework.js",
-    "Build/Xex Crypto Full Version WebGL Build-V(0.4.17) 28-11-2025.data",
-    "Build/Xex Crypto Full Version WebGL Build-V(0.4.17) 28-11-2025.wasm",
+    "Build/Xex Crypto Full Version WebGL Build-V(0.4.19) 18-12-2025.loader.js",
+    "Build/Xex Crypto Full Version WebGL Build-V(0.4.19) 18-12-2025.framework.js",
+    "Build/Xex Crypto Full Version WebGL Build-V(0.4.19) 18-12-2025.data",
+    "Build/Xex Crypto Full Version WebGL Build-V(0.4.19) 18-12-2025.wasm",
     "TemplateData/style.css"
 
 ];
